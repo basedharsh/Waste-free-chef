9859220299c0691e888ebba6c5ba2ce886cb0d31
MananKabra <manankabra200318@gmail.com>
2023-03-22 13:59:21 +0530

    Order Filter Functions

--- lib/models/price_model.dart
+++ lib/models/price_model.dart
@@ -1,20 +1,27 @@
+import 'dart:ui';
+
 import 'package:equatable/equatable.dart';
+
+Color diselectedPriceColor = Color.fromARGB(255, 200, 165, 253);
+Color selectedPriceColor = Color.fromARGB(255, 250, 125, 253);
 
 class Price extends Equatable {
   final int id;
   final String price;
+  Color boxColor;
 
   Price({
     required this.id,
     required this.price,
+    required this.boxColor
   });
 
   @override
-  List<Object?> get props => [id, price];
+  List<Object?> get props => [id, price, boxColor];
 
   static List<Price> prices = [
-    Price(id: 1, price: '\0'),
-    Price(id: 2, price: 'rs-rs'),
-    Price(id: 3, price: 'rs-rs'),
+    Price(id: 1, price: '\0',boxColor: diselectedPriceColor),
+    Price(id: 2, price: '<200',boxColor: diselectedPriceColor),
+    Price(id: 3, price: '>200',boxColor: diselectedPriceColor),
   ];
 }
