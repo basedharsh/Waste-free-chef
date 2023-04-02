5a6ec3ea27ec7330a641eb71b26719ff94307f51
MananKabra <manankabra200318@gmail.com>
2023-04-02 19:57:56 +0530

    confirm order ui chappa wapas

--- lib/place_order/placeOrder.dart
+++ lib/place_order/placeOrder.dart
@@ -1,12 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_email_sender/flutter_email_sender.dart';
 
 
 
-final PlacedOrderDetails = <String, dynamic>{
+var PlacedOrderDetails = <String, dynamic>{
   "providerId": "",
   "customerId": "",
   "orderId": "",
@@ -47,6 +48,12 @@ class PlaceOrderPage extends StatefulWidget {
 
 class _PlaceOrderPageState extends State<PlaceOrderPage> {
   @override
+  void initState(){
+    PlacedOrderDetails['orderQuantity'] = 1;
+  }
+
+
+  @override
   Widget build(BuildContext context) {
     print(orderDetails.data());
     return MaterialApp(
@@ -65,193 +72,237 @@ class PlaceOrderPageApp extends StatefulWidget {
 }
 
 class _PlaceOrderPageAppState extends State<PlaceOrderPageApp> {
+
+  final mylabelstyle = GoogleFonts.roboto(
+    textStyle: TextStyle(
+      color: Colors.grey.shade900,
+      fontSize: 16,
+    ),
+  );
+
+  final myfocusedborder = OutlineInputBorder(
+    borderSide: BorderSide(
+      color: Color.fromARGB(255, 0, 0, 0),
+      width: 3,
+    ),
+    borderRadius: BorderRadius.circular(30.0),
+    //thickness: 5,
+  );
+
+  final myenabledborder = OutlineInputBorder(
+    borderSide: BorderSide(
+      width: 1,
+      color: Color.fromARGB(255, 0, 0, 0),
+    ),
+    borderRadius: BorderRadius.circular(30.0),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text("Currently Available Food Details"),
-              Image(image: NetworkImage(orderDetails.data()['foodimage']),height: 300),
-              Text("Available Quantity : " + orderDetails.data()['foodnos']),
-              Text("Current Price : " + orderDetails.data()['foodprice']),
-              Text("Expiry Date : " + orderDetails.data()['expirydate']),
-              SizedBox(height: 20),
-              Center(child: Text("Place Order")),
-              SizedBox(height: 20),
-              TextFormField(
-                style: TextStyle(
-                  color: Color.fromARGB(255, 227, 233, 236),
-                ),
-                controller: contactName,
-                decoration: InputDecoration(
-                  hintText: 'Contact Name',
-                  hintStyle: TextStyle(
-                    color: Color.fromARGB(255, 139, 13, 236),
-                    // align the text to the left instead of centered
-                  ),
-                  labelText: 'Contact Name',
-                  labelStyle: TextStyle(
-                    color: Color.fromARGB(255, 139, 13, 236),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Color.fromARGB(255, 230, 230, 230),
+      backgroundColor: Colors.deepPurple.shade100,
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(bottomLeft: Radius.circular(80),bottomRight: Radius.circular(80)),
+                  color: Colors.deepPurple.shade300),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 20,bottom: 30),
+                child: Column(
+                  children: [
+                    Text("Currently Available Food Details",style:
+                    TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                    SizedBox(height: 20),
+                    Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(80)
+                      ),
+                      height: 300,
+                      width: 300,
+                      child: FittedBox(
+                        fit: BoxFit.fill,
+                        child: ClipRRect(
+                          borderRadius:
+                          // Only top left corner
+                          BorderRadius.circular(50),
+                          child: Image(image: NetworkImage(orderDetails.data()['foodimage']))
+                        ),
+                      ),
                     ),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
+                    SizedBox(height: 10),
+                    Text("Available Quantity : " + orderDetails.data()['foodnos'],style: TextStyle(
+                        color: Color.fromARGB(
+                            255, 8, 8, 8),
+                        fontWeight: FontWeight.bold)),
+                    Text("Current Price : " + orderDetails.data()['foodprice'],style: TextStyle(
+                        color: Color.fromARGB(
+                            255, 8, 8, 8),
+                        fontWeight: FontWeight.bold)),
+                    Text("Expiry Date : " + orderDetails.data()['expirydate'],style: TextStyle(
+                        color: Color.fromARGB(
+                            255, 8, 8, 8),
+                        fontWeight: FontWeight.bold)),
+                  ],
                 ),
               ),
-              SizedBox(height: 20),
-              TextFormField(
-                style: TextStyle(
-                  color: Color.fromARGB(255, 227, 233, 236),
-                ),
-                controller: contactNumber,
-                decoration: InputDecoration(
-                  hintText: 'Contact Number',
-                  hintStyle: TextStyle(
-                    color: Color.fromARGB(255, 139, 13, 236),
-                    // align the text to the left instead of centered
-                  ),
-                  labelText: 'Contact Number',
-                  labelStyle: TextStyle(
-                    color: Color.fromARGB(255, 139, 13, 236),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Color.fromARGB(255, 230, 230, 230),
-                    ),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              TextFormField(
-                style: TextStyle(
-                  color: Color.fromARGB(255, 227, 233, 236),
-                ),
-                controller: contactEmail,
-                decoration: InputDecoration(
-                  hintText: 'Contact Email',
-                  hintStyle: TextStyle(
-                    color: Color.fromARGB(255, 139, 13, 236),
-                    // align the text to the left instead of centered
-                  ),
-                  labelText: 'Contact Email',
-                  labelStyle: TextStyle(
-                    color: Color.fromARGB(255, 139, 13, 236),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Color.fromARGB(255, 230, 230, 230),
-                    ),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              Center(child: Text("Quantity : " + PlacedOrderDetails['orderQuantity'].toString())),
-              SliderTheme(
-                data: SliderTheme.of(context).copyWith(  //To only change a few things
-                    thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15),
-                    overlayShape: RoundSliderOverlayShape(overlayRadius: 30),
-                    thumbColor: Color(0xFFEB1555),
-                    activeTrackColor: Colors.white,
-                    inactiveTrackColor: Color(0xFF8D8E98),
-                    overlayColor: Color(0x29EB1555)
-                ),
-                child: Slider(
-                    value: PlacedOrderDetails['orderQuantity'].toDouble(),
-                    min: 1,
-                    max: double.parse(orderDetails.data()['foodnos']),
-                    onChanged: (double quantity){
-                      setState(() {
-                        PlacedOrderDetails['orderQuantity'] = quantity.round();
-                        PlacedOrderDetails['orderPrice'] = ( int.parse(orderDetails.data()['foodprice']) / int.parse(orderDetails.data()['foodnos']) * PlacedOrderDetails['orderQuantity'] ).round();
-                      });
-                    }),
-              ),
-              Center(child: (orderDetails.data()['foodprice'] != "0")?
-              Text("Order Price : " + PlacedOrderDetails['orderPrice'].toString() ):
-              Text("0")),
-              SizedBox(height: 20),
-              Center(child: Text("Contact The Provider Before Placing Order")),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+            ),
+
+            SizedBox(height: 20),
+            Center(child: Text("Place Order",style:
+            TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
+            SizedBox(height: 20),
+
+            Padding(
+              padding: const EdgeInsets.only(left: 10,right: 10),
+              child: Column(
                 children: [
-                  MaterialButton(
-                    onPressed: ()async{
-                      await launchUrl(Uri.parse('https://wa.me/91${orderDetails.data()['providernumber']}?text=Hi! I wanted to place an order'),
-                          mode: LaunchMode.externalApplication);
-                    },
-                    child: Icon(Icons.chat),
-                    color: Colors.pink,
+                  TextFormField(
+                    style: mylabelstyle,
+                    controller: contactName,
+                    decoration: InputDecoration(
+                      hintText: 'Contact Name',
+                      hintStyle: mylabelstyle,
+                      labelText: 'Contact Name',
+                      labelStyle: mylabelstyle,
+                      enabledBorder: myenabledborder,
+                      focusedBorder: myfocusedborder,
+                    ),
                   ),
-                  SizedBox(width: 20),
-                  MaterialButton(
-                    onPressed: ()async{
-                      String emailId;
-                      db.collection("users").doc(orderDetails.data()['providerid']).get().then(
-                              (value) => {
-                              emailId = value.data()!["emailAddress"]
-                              }
-                      );
-                      Email email = Email(
-                        body: 'I would like to place an order for ${orderDetails.data()['foodname']}.'
-                            ' The details of the order as as follows:'
-                            '\nName - ${contactName.text.trim()}'
-                            '\nEmail - ${contactEmail.text.trim()}'
-                            '\nNumber - ${contactNumber.text.trim()}'
-                            '\nQuantity - ${PlacedOrderDetails['orderQuantity']}'
-                            '\nPrice - ${PlacedOrderDetails['orderPrice']}',
-                        subject: 'Mail to Confirm Order',
-                        recipients: ['emailId'],
-                        isHTML: false,
-                      );
-                      await FlutterEmailSender.send(email);
-                    },
-                    child: Icon(Icons.mail),
-                    color: Colors.pink,
+                  SizedBox(height: 20),
+                  TextFormField(
+                    style: mylabelstyle,
+                    controller: contactNumber,
+                    decoration: InputDecoration(
+                      hintText: 'Contact Number',
+                      hintStyle: mylabelstyle,
+                      labelText: 'Contact Number',
+                      labelStyle: mylabelstyle,
+                      enabledBorder: myenabledborder,
+                      focusedBorder: myfocusedborder,
+                    ),
                   ),
-                  SizedBox(width: 20),
-                  MaterialButton(
-                    onPressed: ()async{
-                      bool? res = await FlutterPhoneDirectCaller.callNumber(orderDetails.data()['providernumber']);
-                    },
-                    child: Icon(Icons.phone),
-                    color: Colors.pink,
+
+                  SizedBox(height: 20),
+
+                  TextFormField(
+                    style: mylabelstyle,
+                    controller: contactEmail,
+                    decoration: InputDecoration(
+                      hintText: 'Contact Email',
+                      hintStyle: mylabelstyle,
+                      labelText: 'Contact Email',
+                      labelStyle: mylabelstyle,
+                      enabledBorder: myenabledborder,
+                      focusedBorder: myfocusedborder,
+                    ),
                   ),
                 ],
               ),
+            ),
 
-              MaterialButton(
+            SizedBox(height: 20),
+            Center(child: Text("Quantity : " + PlacedOrderDetails['orderQuantity'].toString())),
+            SliderTheme(
+              data: SliderTheme.of(context).copyWith(  //To only change a few things
+                  thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15),
+                  overlayShape: RoundSliderOverlayShape(overlayRadius: 30),
+                  thumbColor: Colors.deepPurple.shade600,
+                  activeTrackColor: Colors.deepPurple.shade800,
+                  inactiveTrackColor: Colors.deepPurple.shade300,
+                  overlayColor: Colors.deepPurple.shade200
+              ),
+              child: Slider(
+                  value: PlacedOrderDetails['orderQuantity'].toDouble(),
+                  min: 1,
+                  max: double.parse(orderDetails.data()['foodnos']),
+                  onChanged: (double quantity){
+                    setState(() {
+                      PlacedOrderDetails['orderQuantity'] = quantity.round();
+                      PlacedOrderDetails['orderPrice'] = ( int.parse(orderDetails.data()['foodprice']) / int.parse(orderDetails.data()['foodnos']) * PlacedOrderDetails['orderQuantity'] ).round();
+                    });
+                  }),
+            ),
+            Center(child: (orderDetails.data()['foodprice'] != "0")?
+            Text("Order Price : " + PlacedOrderDetails['orderPrice'].toString() ):
+            Text("0")),
+            SizedBox(height: 20),
+            Center(child: Text("Contact The Provider Before Placing Order")),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MaterialButton(
                   onPressed: ()async{
-
-                    PlacedOrderDetails['contactName'] = contactName.text.trim();
-                    PlacedOrderDetails['contactNumber'] = contactNumber.text.trim();
-                    PlacedOrderDetails['contactEmail'] = contactEmail.text.trim();
-                    PlacedOrderDetails['foodImage'] = orderDetails.data()['foodimage'];
-                    PlacedOrderDetails['foodName'] = orderDetails.data()['foodname'];
-
-                    await db
-                        .collection('customerOrder')
-                        .doc(PlacedOrderDetails['orderId'] + PlacedOrderDetails['customerId'])
-                        .set(PlacedOrderDetails)
-                        .onError((e, _) => print("Error In Placing Order: $e"));
-
-
-                    ScaffoldMessenger.of(context)
-                        .showSnackBar(SnackBar(content: Text("Order Placed"),));
+                    await launchUrl(Uri.parse('https://wa.me/91${orderDetails.data()['providernumber']}?text=Hi! I wanted to place an order'),
+                        mode: LaunchMode.externalApplication);
                   },
-                child: Text("Place Order"),
-                color: Colors.pink,
-              )
-            ],
-          ),
+                  child: Icon(Icons.chat),
+                  color: Colors.deepPurple.shade300,
+                ),
+                SizedBox(width: 20),
+                MaterialButton(
+                  onPressed: ()async{
+                    String emailId;
+                    db.collection("users").doc(orderDetails.data()['providerid']).get().then(
+                            (value) => {
+                            emailId = value.data()!["emailAddress"]
+                            }
+                    );
+                    Email email = Email(
+                      body: 'I would like to place an order for ${orderDetails.data()['foodname']}.'
+                          ' The details of the order as as follows:'
+                          '\nName - ${contactName.text.trim()}'
+                          '\nEmail - ${contactEmail.text.trim()}'
+                          '\nNumber - ${contactNumber.text.trim()}'
+                          '\nQuantity - ${PlacedOrderDetails['orderQuantity']}'
+                          '\nPrice - ${PlacedOrderDetails['orderPrice']}',
+                      subject: 'Mail to Confirm Order',
+                      recipients: ['emailId'],
+                      isHTML: false,
+                    );
+                    await FlutterEmailSender.send(email);
+                  },
+                  child: Icon(Icons.mail),
+                  color: Colors.deepPurple.shade300,
+                ),
+                SizedBox(width: 20),
+                MaterialButton(
+                  onPressed: ()async{
+                    bool? res = await FlutterPhoneDirectCaller.callNumber(orderDetails.data()['providernumber']);
+                  },
+                  child: Icon(Icons.phone),
+                  color: Colors.deepPurple.shade300,
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            MaterialButton(
+                onPressed: ()async{
+
+                  PlacedOrderDetails['contactName'] = contactName.text.trim();
+                  PlacedOrderDetails['contactNumber'] = contactNumber.text.trim();
+                  PlacedOrderDetails['contactEmail'] = contactEmail.text.trim();
+                  PlacedOrderDetails['foodImage'] = orderDetails.data()['foodimage'];
+                  PlacedOrderDetails['foodName'] = orderDetails.data()['foodname'];
+
+                  await db
+                      .collection('customerOrder')
+                      .doc(PlacedOrderDetails['orderId'] + PlacedOrderDetails['customerId'])
+                      .set(PlacedOrderDetails)
+                      .onError((e, _) => print("Error In Placing Order: $e"));
+
+
+                  ScaffoldMessenger.of(context)
+                      .showSnackBar(SnackBar(content: Text("Order Placed"),));
+                },
+              height: 50,
+              child: Text("Place Order"),
+              color: Colors.deepPurple.shade300,
+            )
+          ],
         ),
       )
     );
